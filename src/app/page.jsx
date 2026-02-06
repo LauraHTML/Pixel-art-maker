9d4fff7449f8401238b834ae0d18d82e60b208d3
LauraHTML <laura.senai03@gmail.com>
2026-02-06 18:38:50 -0300

    Componente PixelArtMaker já está funcionando, adição de fonte

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import PixelArtMaker from "@/components/pixelArtMaker";
 
 export default function Home() {
+  
   return (
     <>
     <main className="min-h-screen flex flex-col items-center justify-between p-24">
--- src/components/pixelArtMaker.jsx
+++ src/components/pixelArtMaker.jsx
@@ -1,6 +1,29 @@
+"use client";
+import { useState } from "react";
 
 export default function PixelArtMaker() {
+    const [tamanhoGrid, setTamanhoGrid] = useState(16);
+    const cor = "#64c6f7"
+    const [grid, setGrid] = useState(Array(tamanhoGrid * tamanhoGrid).fill(("#ffffff")));
+
+    const handleClickPixel = (index) =>{
+        const novaGrid = [...grid];
+        novaGrid[index] = cor;
+        setGrid(novaGrid)
+    }
+
     return (<>
-    <p>pixel art</p>
+    <h1>Pixel art maker</h1>
+    <div className="grid w-120 h-120" style={{ gridTemplateColumns: `repeat(${tamanhoGrid}, 1fr)`}}>
+        
+        {grid.map((cor, index) => (
+           <div className="border"
+           key={index}
+            onClick={() => handleClickPixel(index)}
+            style={{backgroundColor: cor}}
+           >
+        </div> 
+        ))}       
+    </div>
     </>)
 }
